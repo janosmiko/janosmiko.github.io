2fbd093541e47fe60cd9e643ed34fcaccab50e42
Janos Miko <mixe3y@gmail.com>
2023-03-02 11:57:19 +0100

    update dark theme

--- static/js/custom.js
+++ static/js/custom.js
@@ -24,3 +24,18 @@ hiddenElementsRight.forEach((el) => observer.observe(el));
 
 const hiddenElementsContentP = document.querySelectorAll('.content');
 hiddenElementsContentP.forEach((el) => observer.observe(el));
+
+// The function actually applying the offset
+function offsetAnchor() {
+    if(location.hash.length !== 0) {
+        window.scrollTo(window.scrollX, window.scrollY - 48);
+    }
+}
+
+// This will capture hash changes while on the page
+window.addEventListener("hashchange", offsetAnchor);
+
+// This is here so that when you enter the page with a hash,
+// it can provide the offset in that case too. Having a timeout
+// seems necessary to allow the browser to jump to the anchor first.
+window.setTimeout(offsetAnchor, 1); // The delay of 1 is arbitrary and may not always work right (although it did in my testing).
